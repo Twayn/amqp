86959674fdc4de40b7dc632c3a91facf1f7f95db
twayn <Twayn@ya.ru>
2022-04-03 17:11:08 +0300

    Mount volume to docker container for h2

--- src/main/kotlin/com/study/amqp/persist/PowResult.kt
+++ src/main/kotlin/com/study/amqp/persist/PowResult.kt
@@ -4,12 +4,13 @@ import java.math.BigDecimal
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 data class PowResult(
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     val id: Long? = null,
     var base: BigDecimal? = null,
